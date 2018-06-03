2f662f1ef760821375c9ac22878ddc6ca5cca185
Pyth Ng <pyth@gmail.com>
2018-06-03 17:28:36 -0400

    week12: updated hw12

--- week12/hw12.py
+++ week12/hw12.py
@@ -1,17 +1,30 @@
-
-def compute_sum_of_even(n):
-    # compute 2 + 4 + 6 + ... + 2*n (even only)
-    z = 0
-    for c in range(1, n+1):
-        None
-    return z
-
+#
+# def compute_sum_of_even(n):
+#     # compute 2 + 4 + 6 + ... + 2*n (even only)
+#     z = 0
+#     for c in range(1, n+1):
+#         print(c*2)
+#         z = z + c*2
+#
+#     return z
+#
+#
+# def compute_sum_of_even_chloe(n):
+#     # compute 2 + 4 + 6 + ... + 2*n (even only)
+#     z = 0
+#     for c in range(1, 2*n+1):
+#         if c % 2 == 0:
+#             print(c)
+#             z = z + c
+#
+#     return z
 
 def compute_sum_of_odd(n):
     # compute 1 + 3 + 5 + .. + (2*n-1) (odd only)
-    z = None
+    z = 0
     for c in range(1, n+1):
-        None
+        print(c*2-1)
+        z = z + c*2-1
     return z
 
 # ATTN: you do NOT need code in is_first_digit_4() and is_last_digit_4()
@@ -31,8 +44,8 @@ def sum_skipping_4(m):
     # e.g. m = 51 => 1 + 2 + 3 + 5 + 6 + .. + 13 + 15 + 16 ... + 38 + 39 + 50 + 51
     # you can assume 1 <= m <= 99
     s = 0
-    for c in range(None):
-        if None:
+    for c in range(1, m+1):
+        if is_first_digit_4(c) == False and is_last_digit_4(c) == False:
             s = s + c
     return s
 
@@ -41,7 +54,7 @@ def sum_skipping_4(m):
 def get_opposite(pt):
     # compute the opposite pt.
     # e.g. (3, 4) -> (-3, -4); (-2, -3) -> (2, 3)
-    pt2 = (None, None)
+    pt2 = (0-pt[0], 0-pt[1])
     return pt2
 
 
@@ -61,5 +74,17 @@ def calc_area(pt_bottom_left, pt_top_right):
     area = None * height
     return area
 
+if __name__ == "__main__":
+    print("sum of even is")
+    # print(compute_sum_of_even_chloe(3))
+    # print(compute_sum_of_even(3))
 
 
+    #print("sum of odd is")
+    #print(compute_sum_of_odd(4))
+
+    # for m in range(1, 99):
+    #     print(m, "-> ", sum_skipping_4(m))
+
+    print("opposites")
+    print(get_opposite((3, -4)))
